1573c2b8020bfc6d2a809d53a4b663be2c64dc4e
Bruno_Sorrilha <124791792+BrunoSorrilha@users.noreply.github.com>
2024-02-29 08:09:05 -0300

    Add files via upload

---
+++ Aula-3/js/calcula.js
@@ -0,0 +1,7 @@
+var resultado = document.getElementById('resultado')
+
+function peganome(){
+    var nome = document.getElementById('nome').value
+    console.log('nome', nome)
+    resultado.innerHTML = nome
+}
