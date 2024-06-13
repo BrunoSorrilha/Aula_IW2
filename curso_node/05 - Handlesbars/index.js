cafb2cec40a611f4cf0d741aa4c8e21b8c070097
BrunoSorrilha <bruninhodepa10@gmail.com>
2024-06-13 11:16:15 -0300

    aa

--- curso_node/05 - Handlesbars/index.js
+++ curso_node/05 - Handlesbars/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
 //Config
     // Template
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine','handlebars')
+
+    //Conexão Banco de Dados
+const sequelize = new Sequelize('test','root','',{
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+// Rotas
+app.get('/cadastro',function(req,res){
+    res.render('formulario')
+})
+
+app.post('/add', function(req,res){
+    res.send('FORMULARIO RECEBIDO')
+})
 
 
 app.listen(8081,function(){
