7dfc61ecb6272a1e13cc4d78ddff8b0456320ba4
BrunoSorrilha <bruninhodepa10@gmail.com>
2024-06-13 09:54:29 -0300

    enviar

---
+++ curso_node/05 - Handlesbars/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+
+//Config
+    // Template
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine','handlebars')
+
+
+app.listen(8081,function(){
+    console.log("Servidor rodando na url http://localhost:8081")
+})
